09ba99cc17248257e0ae3a3e03e44c1c1825a4cf
Margarita Mkrtchyan <margarita_mkrtchyan@epam.com>
2024-12-04 15:20:13 +0400

    fix index.js

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -1,72 +1,55 @@
 const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const moment = require('moment')
 
-exports.handler = async (event) => {
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const Table_Name = process.env.target_table
+
+exports.handler = async (event,context) => {
+    
     console.log('Received event:', JSON.stringify(event, null, 2));
 
     try {
-        let body;
+        const {principalId,content} = event
+    
+        const freshId = uuid.v4();
+        const freshTime = moment.utc().toISOString();
    
-        if (event.body) {
-            console.log('Triggered by API Gateway');
-            body = JSON.parse(event.body);
-        } else {
-            console.log('Direct Invocation');
-            body = event;
-        }
-
-        const { principalId, content } = body;
-      
-        console.log('Cooontent',content)
-
-        if (!principalId || typeof content !== 'object') {
-            console.error('Validation failed:', { principalId, content });
-            return {
-                statusCode: 400,
-                body: JSON.stringify({
-                    message: 'Invalid input. "principalId" must be a number and "content" must be a JSON object.',
-                }),
-                headers: { 'Content-Type': 'application/json' }
-            };
-        }
-      
+   
         const newItem = {
-            TableName: process.env.target_table,
+            TableName: Table_Name,
             Item: {
-                id: uuidv4(),
-                principalId: Number(principalId),
-                createdAt: new Date().toISOString(),
+                id: freshId,
+                principalId: principalId,
+                createdAt: freshTime,
                 body: content,
             },
         };
+
+        await dynamoDB.put(item).promise();
+
+        const newObject = {
+            TableName: Table_Name,
+            Item: {
+                id: freshId,
+                principalId: principalId,
+                createdAt: freshTime,
+                body: content,
+            },
+        }
+
+        return {
+            statusCode:201,
+            body:JSON.stringify({event:newObject})
+        };
+        } catch (error) {
+         console.log("Eror during request:", error)
+         return {
+            statusCode:500,
+            body:JSON.stringify({error:"Server Error"})
+         }
+        }
        
 
-        console.log('Using DynamoDB table:', process.env.TARGET_TABLE);
-        console.log('Writing item:', JSON.stringify(newItem, null, 2));
-
-        await dynamoDB.put(newItem).promise();
-
-        console.log('Successfully wrote item to DynamoDB.');
-
-        return {
-            statusCode: 201,
-            body: JSON.stringify({
-                message: 'Event stored successfully',
-                item: newItem.Item,
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        };
-    } catch (error) {
-        console.error('Error occurred while storing event:', error);
-
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: 'Error storing event',
-                error: error.message,
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        };
     }
-};
+
