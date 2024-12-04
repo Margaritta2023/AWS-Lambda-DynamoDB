e1df6a26c7e48881ca30e4043119fdd353d623f0
Margarita Mkrtchyan <margarita_mkrtchyan@epam.com>
2024-12-04 10:49:18 +0400

    fix index.json

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -1,19 +1,17 @@
 const AWS = require('aws-sdk');
-const { v4: uuidv4 } = require('uuid'); 
+const { v4: uuidv4 } = require('uuid');
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    console.log('Received event:', JSON.stringify(event, null, 2)); 
+    console.log('Received event:', JSON.stringify(event, null, 2));
 
     try {
         let body;
 
-     
         if (event.body) {
             console.log('Triggered by API Gateway');
             body = JSON.parse(event.body);
         } else {
-
             console.log('Direct Invocation');
             body = event;
         }
@@ -27,45 +25,45 @@ exports.handler = async (event) => {
                 body: JSON.stringify({
                     message: 'Invalid input. "principalId" must be a number and "content" must be a JSON object.',
                 }),
+                headers: { 'Content-Type': 'application/json' }
             };
         }
 
-        
         const newItem = {
-            TableName: process.env.TARGET_TABLE, 
+            TableName: process.env.TARGET_TABLE,
             Item: {
-                id: uuidv4(), 
-                principalId: Number(principalId), 
-                createdAt: new Date().toISOString(), 
-                body: content, 
+                id: uuidv4(),
+                principalId: Number(principalId),
+                createdAt: new Date().toISOString(),
+                body: content,
             },
         };
 
-        console.log('Writing item to DynamoDB:', newItem);
+        console.log('Using DynamoDB table:', process.env.TARGET_TABLE);
+        console.log('Writing item:', JSON.stringify(newItem, null, 2));
 
-      
         await dynamoDB.put(newItem).promise();
 
         console.log('Successfully wrote item to DynamoDB.');
 
-     
         return {
             statusCode: 201,
             body: JSON.stringify({
                 message: 'Event stored successfully',
                 item: newItem.Item,
             }),
+            headers: { 'Content-Type': 'application/json' }
         };
     } catch (error) {
         console.error('Error occurred while storing event:', error);
 
-        // Return an error response
         return {
             statusCode: 500,
             body: JSON.stringify({
                 message: 'Error storing event',
                 error: error.message,
             }),
+            headers: { 'Content-Type': 'application/json' }
         };
     }
 };
